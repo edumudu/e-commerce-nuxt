2102fa34e496fedd4afcf1dd6d2d91fbf5e9f461
Eduardo Wesley <eduardomudutiu@gmail.com>
2021-03-19 00:05:41 -0300

    chore: move from node-sass to dart-sass

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: 'stylelint-config-standard-scss',
   rules: {
     'string-quotes': 'single',
+    'selector-pseudo-element-no-unknown': [
+      true,
+      { ignorePseudoElements: ['v-deep'] },
+    ],
   },
 };
